f9d8a7e66794938c73bbd6f72fd731ee976e31a5
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2025-04-13 13:21:29 +0200

    Add styles for code samples

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import 'reveal.js/dist/reveal.css';
 
 import Reveal from 'reveal.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
 
 const deck = new Reveal();
-deck.initialize();
+deck.initialize({
+  plugins: [RevealHighlight],
+  transition: 'none',
+});
